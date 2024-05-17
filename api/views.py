67f0e2bc3bff44bf4137abcea5fcc7e2bda07b60
zixeel <strpan549@gmail.com>
2024-05-17 17:47:46 +0300

    fix

--- api/views.py
+++ api/views.py
@@ -1,11 +1,7 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, status
-from django.db.models import Count, Q, F
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from api.models import FoodCategory, Food
+from api.models import FoodCategory
 from api.serializers import FoodListSerializer
 
 
